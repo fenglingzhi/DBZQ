965af976b57460d49c6563a54490bc7d4bb65292
yanmeng <513683375@qq.com>
2019-09-07 16:43:35 +0800

    mock test

--- src/mock/test.js
+++ src/mock/test.js
@@ -4,7 +4,30 @@ export default {
     if (params.query.indexOf('test(')) {
       return {
         query: {
-          test: 'Hell world'
+          test: 'Hell world',
+          title:'This is test title'
+          arr:[
+            'test arr1',
+            {
+              name:'test',
+              val:'1'
+            }
+          ],
+          obj:{
+            name:'test',
+            arr:[
+              {
+                name:'obj1',
+                value:1,
+                children:[
+                  {
+                    name:'boj_child',
+                    val:'obj_c1'
+                  }
+                ]
+              }
+            ]
+          }
         }
       }
     }
