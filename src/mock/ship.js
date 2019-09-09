1c64535ff5986d0272917fff26d85d86b7b07036
YanM <513683375@qq.com>
2019-09-09 23:31:51 +0800

    船舶信息完数据模型完成

--- src/mock/ship.js
+++ src/mock/ship.js
@@ -22,16 +22,18 @@ export default {
         },
         // 航班信息
         real_time_info: {
-          take_off_base: '起飞基地',
-          landing_base: '降落基地',
-          start_time: '2013-09-01 13:41:52', // 出发时间
-          pre_time: '2013-09-01 13:41:52', // 预计到达时间
+          heading: '船首向',
+          ending: '船迹向',
+          speed: '航速', // 出发时间
+          status: '状态（在航等）',
           longitude: '123.123123123', // 经度
           latitude: '123.123123123', // 纬度
-          height: '123123', // 高度
-          horizontal_velocity: '水平速度',
-          vertical_speed: '垂直速度',
-          azimuth: '方位角'
+          draught: '123123', // 吃水
+          start_port: '始发港口',
+          mid_port: '停靠港口',
+          end_port: '目的港口',
+          time: '预到时间',
+          task: '船舶任务'
         },
         // 人员信息
         usr: {
@@ -61,14 +63,15 @@ export default {
         },
         // 设施-港口
         facility: {
-          name: '机场名称',
-          code: '机场代码',
+          name: '港口名称',
+          code: '港口代码',
           type: '机场类型',
-          country: '所属国家/地区',
-          date: '通航日期',
-          level: '飞行区等级',
-          area: '航站楼面积',
-          num: '机位数量'
+          country: '所属地区',
+          main_good: '主营货类',
+          max_throughput: '最大吞吐量',
+          area: '水域面积',
+          opt_organization: '运行机构',
+          phone: '联系电话'
         }
       }
     }
