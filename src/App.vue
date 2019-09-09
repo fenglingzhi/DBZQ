01e9fc74159f7899db7e0b086756454bb11bdf85
yanmeng <513683375@qq.com>
2019-09-09 21:28:06 +0800

    data init

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,10 @@
     <mapcan name="mainmap" :center="[100,31]" :zoom="4" style="height:100%">
       <tilelayer slot="baselayer" :id="`googlelayer`" :url-template="googleUrl"></tilelayer>
       <tilelayer :id="`googleLabellayer`" :url-template="gglLabelUrl"></tilelayer>
+      <uicomponent :position={top:10,left:10}>
+        <filterwrap></filterwrap>
+      </uicomponent>
       <uicomponent :position={top:10,right:10}>
-        <button>{{test}}</button>
-      </uicomponent>
-      <uicomponent :position={top:10,left:10}>
-        <button>{{test}}</button>
-      </uicomponent>
-      <uicomponent :position={top:10,left:10}>
         <button>{{test}}</button>
       </uicomponent>
       <uicomponent :position={bottom:10,left:10}>
@@ -24,9 +21,10 @@ import ax from 'axios'
 import Mapcan from './components/MapControl'
 import Tilelayer from './components/Tilelayer'
 import Uicomponent from './components/UIComponent'
+import filterwrap from './components/filter.vue'
 export default {
   name: 'app',
-  components: { Mapcan, Tilelayer, Uicomponent },
+  components: { Mapcan, Tilelayer, Uicomponent, filterwrap },
   data() {
     return {
       test: false,
@@ -35,15 +33,10 @@ export default {
       get_obj:{},
     }
   },
-  methods:{
-    getVal(d){
-      alert(d)
-    }
-  },
   methods: {
     googleUrl(x, y, z, s) {
       var burl = '/googlemap/vt?lyrs=s@852&gl=cn'
-      console.log(`${burl}&x=${x}&y=${y}&z=${z}`)
+//      console.log(`${burl}&x=${x}&y=${y}&z=${z}`)
       // return `/googlemap/vt/pb=!1m4!1m3!1i${z}!2i${x}!3i${y}!2m2!1e5!2sshading!2m2!1e6!2scontours!2m3!1e0!2sm!3i480190268!3m8!3scn!5e1105!12m1!1e67!12m1!1e63!12m1!1e3!4e0!5m2!5f2!6b1`
       return `${burl}&x=${x}&y=${y}&z=${z}`
     },
--- src/mock/test.js
+++ src/mock/test.js
@@ -4,41 +4,7 @@ export default {
     if (params.query.indexOf('test(')) {
       return {
         query: {
-          test: 'Hell world',
-          title:'This is test title',
-          arr:[
-            {
-              name:'test1',
-              val:'1'
-            },
-            {
-              name:'test2',
-              val:'2'
-            },
-            {
-              name:'test3',
-              val:'3'
-            },
-            {
-              name:'test4',
-              val:'4'
-            },
-          ],
-          obj:{
-            name:'test',
-            arr:[
-              {
-                name:'obj1',
-                value:1,
-                children:[
-                  {
-                    name:'boj_child',
-                    val:'obj_c1'
-                  }
-                ]
-              }
-            ]
-          }
+          test: 'Hell world'
         }
       }
     }
