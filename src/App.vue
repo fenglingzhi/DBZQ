7d86293a19130158e0ac6dda9bd1c3294fd2575b
YanM <513683375@qq.com>
2019-09-09 23:34:49 +0800

    air data && ship data complate

--- src/App.vue
+++ src/App.vue
@@ -28,18 +28,17 @@ export default {
   data() {
     return {
       test: false,
-      title:'',
-      list1:[],
-      get_obj:{},
+      title: '',
+      list1: [],
+      get_obj: {}
     }
   },
   methods: {
     googleUrl(x, y, z, s) {
       var burl = '/googlemap/vt?lyrs=s@852&gl=cn'
-//      console.log(`${burl}&x=${x}&y=${y}&z=${z}`)
+      // console.log(`${burl}&x=${x}&y=${y}&z=${z}`)
       // return `/googlemap/vt/pb=!1m4!1m3!1i${z}!2i${x}!3i${y}!2m2!1e5!2sshading!2m2!1e6!2scontours!2m3!1e0!2sm!3i480190268!3m8!3scn!5e1105!12m1!1e67!12m1!1e63!12m1!1e3!4e0!5m2!5f2!6b1`
       return `${burl}&x=${x}&y=${y}&z=${z}`
-      // return `/googlemap/vt/pb=!1m4!1m3!1i${z}!2i${x}!3i${y}!2m2!1e5!2sshading!2m2!1e6!2scontours!2m3!1e0!2sm!3i480190268!3m8!3scn!5e1105!12m1!1e67!12m1!1e63!12m1!1e3!4e0!5m2!5f2!6b1`
     },
     gglLabelUrl(x, y, z) {
       return `/googlemap/vt?lyrs=h@852&gl=cn&x=${x}&y=${y}&z=${z}`
@@ -52,9 +51,9 @@ export default {
         test(){}
       }`
     }).then(r => {
-      let resp = r.data.query;
-      this.test = r.data.query.test;
-      vm.title = resp.title;
+      let resp = r.data.query
+      this.test = r.data.query.test
+      vm.title = resp.title
       vm.list1 = resp.arr
     })
   }
--- src/mock/airplane.js
+++ src/mock/airplane.js
@@ -3,20 +3,20 @@ export default {
     let params = JSON.parse(req.body)
     if (params.query.indexOf('test(')) {
       return {
-        // 注释内容为非必须展示字段，后期跟进需求方需求适当添加
+        // 缺资讯信息，
+        // 基础信息
         base_info: {
-          air_name: '飞机名称',
-          air_attribute: '1', // 飞机性质（军用 0 /民用 1）
-          air_model: 'abc_123', // 机型
+          name: '飞机名称',
+          attribute: '1', // 飞机性质（军用 0 /民用 1）
+          model: 'abc_123', // 机型
           sign_info: '注册信息',
           organization: '隶属单位',
           country: '所属国家',
           yd_code: '应答机编码',
           radar_type: '飞机雷达型号',
           ICAO: 'ICAO编号'
-          // msn: 'MSN（序列号）',
-          // first_fly: '首飞（机龄）'
         },
+        // 航班信息
         real_time_info: {
           take_off_base: '起飞基地',
           landing_base: '降落基地',
@@ -28,12 +28,17 @@ export default {
           horizontal_velocity: '水平速度',
           vertical_speed: '垂直速度',
           azimuth: '方位角'
-          // standard_atmosphere_height: '标准大气压高度',
-          // target_height: '目标高度',
-          // update: '数据更新时间',
-          // f: '所在地天气情况',
-          // i: '所在航空管制区'
         },
+        // 动态
+        dynamic: {
+          take_off_base: '起飞基地',
+          landing_base: '降落基地',
+          start_time: '2013-09-01 13:41:52', // 出发时间
+          pre_time: '2013-09-01 13:41:52', // 预计到达时间
+          longitude: '123.123123123', // 经度
+          latitude: '123.123123123' // 纬度
+        },
+        // 人员信息
         usr: {
           name: '姓名',
           nation: '民族',
@@ -46,6 +51,7 @@ export default {
           edu: '学历',
           city: '常在城市'
         },
+        // 机构信息
         organization: {
           name: '机构名称',
           foreign_name: '外文名称',
@@ -57,6 +63,17 @@ export default {
           superior_org: '上级机构',
           leader: '主管领导',
           site: '机构网站'
+        },
+        // 设施-机场
+        facility: {
+          name: '机场名称',
+          code: '机场代码',
+          type: '机场类型',
+          country: '所属国家/地区',
+          date: '通航日期',
+          level: '飞行区等级',
+          area: '航站楼面积',
+          num: '机位数量'
         }
       }
     }
