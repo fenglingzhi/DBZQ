f3b2e51806e96373c4b00083bb44c03aec766e08
signhuwei <22626612@qq.com>
2019-10-18 14:12:23 +0800

    base tile set

--- src/components/Tilelayer.vue
+++ src/components/Tilelayer.vue
@@ -26,36 +26,46 @@ export default {
   watch: {
     hide(n, o) {
       n ? this.mapItem.hide() : this.mapItem.show()
+    },
+    urlTemplate(n, o) {
+      if(n && o){
+        this.mapItem.remove();
+        this.mapItem = new mapcan.TileLayer(this.id, this.createLayerConfig())
+        this.base ? this.container.map.setBaseLayer(this.$layer) : this.container.map.addLayer(this.$layer)
+      }
     }
   },
   methods: {
     parseArcgisServerConfig(url) {
       return new Promise((resolve, reject) =>
         mapcan.SpatialReference.loadArcgis(url, (err, conf) => err ? reject(err) : resolve(conf)))
+    },
+    createLayerConfig() {
+      let layerCfg = {
+        visible: !this.hide,
+        // tileSystem: this.tileSystem,
+        // tileSize: this.tileSize,
+        urlTemplate: this.urlTemplate,
+        subdomains: this.subDomains
+      }
+      if (this.arcgisUrl) {
+        // debugger
+        let mapConfig = await this.parseArcgisServerConfig(`${this.arcgisUrl}?f=pjson`)
+        layerCfg.tileSystem = mapConfig.tileSystem
+        // this.tileSystem[3] = 2.0037508342787E7
+        layerCfg.tileSize = mapConfig.tileSize
+        layerCfg.zIndex = this.zIndex
+      } else {
+        layerCfg.SpatialReference = { projection: 'EPSG:3857' }
+        layerCfg.tileSystem = mapcan.TileSystem.getDefault({ code: 'google' })
+        layerCfg.zIndex = this.zIndex
+      }
+      return layerCfg
     }
   },
   async mounted() {
     // debugger
-    let layerCfg = {
-      visible: !this.hide,
-      // tileSystem: this.tileSystem,
-      // tileSize: this.tileSize,
-      urlTemplate: this.urlTemplate,
-      subdomains: this.subDomains
-    }
-    if (this.arcgisUrl) {
-      // debugger
-      let mapConfig = await this.parseArcgisServerConfig(`${this.arcgisUrl}?f=pjson`)
-      layerCfg.tileSystem = mapConfig.tileSystem
-      // this.tileSystem[3] = 2.0037508342787E7
-      layerCfg.tileSize = mapConfig.tileSize
-      layerCfg.zIndex = this.zIndex
-    } else {
-      layerCfg.SpatialReference = { projection: 'EPSG:3857' }
-      layerCfg.tileSystem = mapcan.TileSystem.getDefault({ code: 'google' })
-      layerCfg.zIndex = this.zIndex
-    }
-    this.mapItem = new mapcan.TileLayer(this.id, layerCfg)
+    this.mapItem = new mapcan.TileLayer(this.id, this.createLayerConfig())
     // this.base ? this.container.map.setBaseLayer(this.$layer) : this.container.map.addLayer(this.$layer)
   }
 }
