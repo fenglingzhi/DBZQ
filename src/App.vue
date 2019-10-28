55532b59933a5b848fc2e47ae1162bff561df82b
signhuwei <22626612@qq.com>
2019-10-28 12:16:03 +0800

    test push

--- src/App.vue
+++ src/App.vue
@@ -683,6 +683,7 @@ export default {
           lineSymbol: { lineColor: { type: 'linear', colorStops: [ [0.00, 'white'], [1 / 4, 'aqua'], [2 / 4, 'green'], [3 / 4, 'orange'], [1.00, 'red'] ] } }
         }
         this.playStatus = 'play'
+        
         this.detailchar = { path: e.track.map(p => ([ p.alt, p.timestamp ])), unitTime }
       }, 1000)
     })
