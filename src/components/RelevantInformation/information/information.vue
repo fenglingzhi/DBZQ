2e30d59dd843dc0e5152184ca184c09f496dd12e
X-Toby <15651960660@163.com>
2019-11-02 19:38:15 +0800

    咨询内容超出 提示框

--- src/components/RelevantInformation/information/information.vue
+++ src/components/RelevantInformation/information/information.vue
@@ -32,6 +32,31 @@
     border-radius: 0;
     background: rgba(0,0,0,0.1);
   }
+  .ivu-tooltip-inner-with-width::-webkit-scrollbar {/*滚动条整体样式*/
+    width: 4px;     /*高宽分别对应横竖滚动条的尺寸*/
+    height: 4px;
+  }
+  .ivu-tooltip-inner-with-width::-webkit-scrollbar-thumb {/*滚动条里面小方块*/
+    border-radius: 5px;
+    -webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);
+    background: rgba(0,0,0,0.2);
+  }
+  .ivu-tooltip-inner-with-width::-webkit-scrollbar-track {/*滚动条里面轨道*/
+    -webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);
+    border-radius: 0;
+    background: rgba(0,0,0,0.1);
+  }
+  .ivu-tooltip-inner-with-width {
+    max-width: 300px !important;
+    max-height: 500px;
+    overflow: auto
+  }
+  .ivu-tooltip-rel{
+    text-overflow:ellipsis;
+    white-space: nowrap;
+    max-width: 240px;
+    overflow: hidden;
+  }
 </style>
 <template>
     <!-- 航班设施信息 -->
@@ -53,14 +78,27 @@ export default {
         },
         {
           title: '内容',
-          key: 'content'
+          width: 250,
+          key: 'content',
+          ellipsis: true,
+          tooltip: true,
+          render: (h, params) => {
+            return h('Tooltip', {
+                props: { placement: 'top', 'max-width': '250px', transfer: true },
+            }, [
+                params.row.content,
+                h('span', { class:'tooltipTable', slot: 'content', style: { whiteSpace: 'normal', wordBreak: 'break-all',width: '300px'} },params.row.content)
+            ])
+          }
         },
         {
           title: '来源',
+          maxWidth: 100,
           key: 'source'
         },
         {
           title: '发布时间',
+          maxWidth: 100,
           key: 'timestamp'
         }
       ]
